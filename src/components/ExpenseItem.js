b7cc409c3ab5f81cc5f4e61d774bff06c3bc1657
ande-gl <and.g.000@gmail.com>
2022-06-22 14:59:54 +0500

    Splitting Components into Multiple Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (myprops) => {
   // const {dta, ttl, amnt} = myprops
-  const day = myprops.dta.toLocaleString('en-US', {day:'2-digit'})
-  const month = myprops.dta.toLocaleString('en-US', {month:'long'})
-  const year = myprops.dta.getFullYear()
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate dta={myprops.dta} />
       <div className="expense-item__description">
         <h2>{myprops.ttl}</h2>
         <div className="expense-item__price">{myprops.amnt}</div>

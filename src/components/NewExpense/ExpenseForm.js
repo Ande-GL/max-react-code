a088420a7f4bbc0eba93b669f400dd5d88ede140
Ande-GL <Ande-GL@users.noreply.github.com>
2022-07-02 22:41:25 +0500

    add comment

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,7 +4,9 @@ import "./ExpenseForm.css";
 const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const titleChangeHandler = (event) => {
+    // we get "event" object atomatically. We don't need to do anything to get this
     setEnteredTitle(event.target.value);
+    // event object continue a banch of element (we can see it throgh the console.log(event) but we need a special "event.target.value")
   };
   const [enteredAmont, setEnteredAmount] = useState("");
   const amountChangeHandler = (event) => {
@@ -17,27 +19,32 @@ const ExpenseForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+    // prevent default browsers behaver - reload page after form submitted (after ckick button)
 
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmont,
       data: new Date(enteredDate),
     };
+    // create an object with our value
     console.log(expenseData);
+
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+    // for reset value in the input fields - after creation object reinitiate it with empty string, as it was
   };
 
   return (
-    <form onSubmit={submitHandler}> 
-    {/* we use onSubmit instead onClick in button */}
+    <form onSubmit={submitHandler}>
+      {/* we use onSubmit instead onClick in button */}
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
           <input
             type="text"
             value={enteredTitle}
+            // for reset value in the input field - adding value attribute and bind this to enteredTitle (Adding Two-Way Binding) - so we also feed the state back into the input (so that when we change the state, we also change input)
             onChange={titleChangeHandler}
             // we also can use onImput, but onChange event - for all inputs types
           />
